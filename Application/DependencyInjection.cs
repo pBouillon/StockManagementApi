422128484c32d83cf0daf1b423b9feae937d7666
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-12-28 15:17:19 +0100

    Setup the validation pipeline in the DI container

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using Application.Commons.Behaviors;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
@@ -18,6 +20,24 @@ namespace Application
         public static void AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.ConfigureMediatrPipeline();
+        }
+
+        /// <summary>
+        /// Setup the MediatR validation pipeline
+        /// </summary>
+        /// <remarks>
+        /// The pipeline will be executed in the same order as set up below (from the first one to the last)
+        /// </remarks>
+        /// <param name="services">
+        /// <see cref="IServiceCollection"/> used to setup the dependency injection container
+        /// </param>
+        private static void ConfigureMediatrPipeline(this IServiceCollection services)
+        {
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
         }
     }
 }
