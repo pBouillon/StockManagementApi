ebfba454df221d32bbb494cbae72326dd1f6b336
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-12-28 15:19:32 +0100

    Add the UpdateProductCommand validator

---
+++ Application/Products/Commands/UpdateProductCommand/UpdateProductCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using FluentValidation;
+
+namespace Application.Products.Commands.UpdateProductCommand
+{
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(product => product.Name)
+                .NotEmpty()
+                .WithMessage("A product should have a name")
+                .MaximumLength(Product.NameMaxLength)
+                .WithMessage($"The product's name cannot exceed {Product.NameMaxLength} characters");
+        }
+    }
+}
